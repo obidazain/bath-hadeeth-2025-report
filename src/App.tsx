c31dd61bc80773cd0d33cc61b8501166ec7261a0
obida <obidazain95@gmail.com>
2025-12-22 20:10:08 +0300

    إضافة شريط عنوان مرئي للتشخيص

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import { PresentationPage } from './pages/PresentationPage';
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<PresentationPage />} />
-      </Routes>
-    </HashRouter>
+    <>
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        background: '#2383e2',
+        color: 'white',
+        padding: '10px',
+        zIndex: 9999,
+        textAlign: 'center',
+        direction: 'rtl'
+      }}>
+        التقرير السنوي - بث حديث 2025
+      </div>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<PresentationPage />} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
